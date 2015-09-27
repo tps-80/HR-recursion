1d37e32ed4ca9045db939fd8925178b3f214e532
Thomas Sorensen <thomaspsorensen@gmail.com>
2015-09-27 01:49:06 -0700

    pushes to result but stops on #text nodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var results = [];
+var getElementsByClassName = function(className, node){
+  //create results array
+  
+  var node = node || document.body;
+
+  if(node.className === className) {
+    results.push(node);
+  }
+
+  // console.log(node.childNodes);
+  if(node.hasChildNoes) {
+     for(var i = 0; i < node.childNodes.length; i++) {
+      getElementsByClassName(className, node.childNodes[i]);
+  }
+
+  }
+  
+  //loop through all nodes of document.body
+  //if node class === classname 
+    //add node to a result array
+  //find all the childnodes of current node
+    //invoke getElements again on each child node
+  //if there are no more nodes, move on to next parent node
+
+   //return results array
+   return results;
 };
